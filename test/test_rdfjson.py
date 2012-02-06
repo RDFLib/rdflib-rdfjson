74d11f698015d78d553ea1a7d09b98a60a322ec7
Graham Higgins <gjh@bel-epa.com>
2012-02-06 12:50:32 +0000

    Adjust jython test to reveal parser problem.

--- test/test_rdfjson.py
+++ test/test_rdfjson.py
@@ -78,18 +78,275 @@ class RdfJsonSerializerTestCase(unittest.TestCase):
         g2 = Graph()
         g2.parse(data=rdf_data)
         self.assert_(self.graph.isomorphic(g2))
-        # log.debug("XML")
-        # log.debug(res)
-        self.assert_('''rdf:nodeID="na's Homepage"''' not in res)
+        log.debug("XML")
+        log.debug(res)
+        self.assert_('''rdf:nodeID="na's Homepage"''' not in res and '''rdf:nodeID="na Wilder"''' not in res)
 
     def test_roundtrip_json(self):
         self.graph.parse(data=json_data, format="rdf-json")
         res = self.graph.serialize(format="rdf-json")
         g2 = Graph()
         g2.parse(data=res, format="rdf-json")
-        # log.debug("RDFJSON")
-        # log.debug(res)
+        log.debug("RDFJSON")
+        log.debug(res)
         self.assert_(len(self.graph) == 12 and len(g2) == 12)
         self.assert_(self.graph.isomorphic(g2))
-        self.assert_('''"_:na's Homepage"''' not in res)
+        self.assert_('''"_:na's Homepage"''' not in res and '''"value": "_:na Wilder"''' not in res)
 
+
+"""
+Result of logging:
+
+======================================================================
+FAIL: test_roundtrip_json (test.test_rdfjson.RdfJsonSerializerTestCase)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File ".../rdflib-rdfjson/test/test_rdfjson.py", line 94, in test_roundtrip_json
+    self.assert_('''"_:na's Homepage"''' not in res)
+AssertionError: 
+-------------------- >> begin captured logging << --------------------
+test.test_rdfjson: DEBUG: RDFJSON
+test.test_rdfjson: DEBUG: {
+  "_:person": {
+    "http://xmlns.com/foaf/0.1/mbox_sha1sum": [
+      {
+        "type": "literal",
+        "value": "69e31bbcf58d432950127593e292a55975bc66fd"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/homepage": [
+      {
+        "type": "uri",
+        "value": "http://example.org/about"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/firstName": [
+      {
+        "type": "literal",
+        "value": "Anna"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/depiction": [
+      {
+        "type": "uri",
+        "value": "http://example.org/pic.jpg"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/name": [
+      {
+        "type": "literal",
+        "value": "Anna Wilder"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/surname": [
+      {
+        "type": "literal",
+        "value": "Wilder"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/nick": [
+      {
+        "type": "literal",
+        "value": "wildling"
+      },
+      {
+        "type": "literal",
+        "value": "wilda"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/made": [
+      {
+        "type": "uri",
+        "value": "http://example.org/about"
+      }
+    ]
+  },
+  "http://example.org/about": {
+    "http://purl.org/dc/elements/1.1/creator": [
+      {
+        "type": "literal",
+        "value": "Anna Wilder"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/maker": [
+      {
+        "type": "bnode",
+        "value": "_:na's Homepage"
+      }
+    ],
+    "http://purl.org/dc/elements/1.1/title": [
+      {
+        "lang": "en",
+        "type": "literal",
+        "value": "Anna's Homepage"
+      }
+    ]
+  }
+}
+--------------------- >> end captured logging << ---------------------
+
+======================================================================
+FAIL: test_serialize_xml (test.test_rdfjson.RdfJsonSerializerTestCase)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File ".../rdflib-rdfjson/test/test_rdfjson.py", line 83, in test_serialize_xml
+    self.assert_('''rdf:nodeID="na's Homepage"''' not in res)
+AssertionError: 
+-------------------- >> begin captured logging << --------------------
+test.test_rdfjson: DEBUG: XML
+test.test_rdfjson: DEBUG: <?xml version="1.0" encoding="UTF-8"?>
+<rdf:RDF
+   xmlns:ns1="http://xmlns.com/foaf/0.1/"
+   xmlns:ns2="http://purl.org/dc/elements/1.1/"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+>
+  <rdf:Description rdf:about="http://example.org/about">
+    <ns1:maker rdf:nodeID="na's Homepage"/>
+    <ns2:creator>Anna Wilder</ns2:creator>
+    <ns2:title xml:lang="en">Anna's Homepage</ns2:title>
+  </rdf:Description>
+  <rdf:Description rdf:nodeID="person">
+    <ns1:firstName>Anna</ns1:firstName>
+    <ns1:mbox_sha1sum>69e31bbcf58d432950127593e292a55975bc66fd</ns1:mbox_sha1sum>
+    <ns1:made rdf:resource="http://example.org/about"/>
+    <ns1:surname>Wilder</ns1:surname>
+    <ns1:nick>wildling</ns1:nick>
+    <ns1:nick>wilda</ns1:nick>
+    <ns1:name>Anna Wilder</ns1:name>
+    <ns1:depiction rdf:resource="http://example.org/pic.jpg"/>
+    <ns1:homepage rdf:resource="http://example.org/about"/>
+  </rdf:Description>
+</rdf:RDF>
+
+--------------------- >> end captured logging << ---------------------
+
+----------------------------------------------------------------------
+Ran 4 tests in 0.425s
+
+"""
+
+"""
+Ah so, slightly different results for Jython output which escapes the over-precise test
+
+======================================================================
+FAIL: test_roundtrip_json (test.test_rdfjson.RdfJsonSerializerTestCase)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "/home/gjh/.virtualenvs/rdflib/src/rdflib-rdfjson/test/test_rdfjson.py", line 96, in test_roundtrip_json
+    self.assert_(True is False) # Force failure in order to trigger logging
+AssertionError: 
+-------------------- >> begin captured logging << --------------------
+test.test_rdfjson: DEBUG: RDFJSON
+test.test_rdfjson: DEBUG: {
+  "http://example.org/about": {
+    "http://purl.org/dc/elements/1.1/creator": [
+      {
+        "type": "literal",
+        "value": "Anna Wilder"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/maker": [
+      {
+        "type": "bnode",
+        "value": "_:na Wilder"
+      }
+    ],
+    "http://purl.org/dc/elements/1.1/title": [
+      {
+        "lang": "en",
+        "type": "literal",
+        "value": "Anna's Homepage"
+      }
+    ]
+  },
+  "_:person": {
+    "http://xmlns.com/foaf/0.1/name": [
+      {
+        "type": "literal",
+        "value": "Anna Wilder"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/mbox_sha1sum": [
+      {
+        "type": "literal",
+        "value": "69e31bbcf58d432950127593e292a55975bc66fd"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/firstName": [
+      {
+        "type": "literal",
+        "value": "Anna"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/made": [
+      {
+        "type": "uri",
+        "value": "http://example.org/about"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/homepage": [
+      {
+        "type": "uri",
+        "value": "http://example.org/about"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/depiction": [
+      {
+        "type": "uri",
+        "value": "http://example.org/pic.jpg"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/nick": [
+      {
+        "type": "literal",
+        "value": "wilda"
+      },
+      {
+        "type": "literal",
+        "value": "wildling"
+      }
+    ],
+    "http://xmlns.com/foaf/0.1/surname": [
+      {
+        "type": "literal",
+        "value": "Wilder"
+      }
+    ]
+  }
+}
+--------------------- >> end captured logging << ---------------------
+
+======================================================================
+FAIL: test_serialize_xml (test.test_rdfjson.RdfJsonSerializerTestCase)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "/home/gjh/.virtualenvs/rdflib/src/rdflib-rdfjson/test/test_rdfjson.py", line 84, in test_serialize_xml
+    self.assert_(True is False) # Force failure in order to trigger logging
+AssertionError: 
+-------------------- >> begin captured logging << --------------------
+test.test_rdfjson: DEBUG: XML
+test.test_rdfjson: DEBUG: <?xml version="1.0" encoding="UTF-8"?>
+<rdf:RDF
+   xmlns:ns1="http://xmlns.com/foaf/0.1/"
+   xmlns:ns2="http://purl.org/dc/elements/1.1/"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+>
+  <rdf:Description rdf:about="http://example.org/about">
+    <ns2:creator>Anna Wilder</ns2:creator>
+    <ns1:maker rdf:nodeID="na Wilder"/>
+    <ns2:title xml:lang="en">Anna's Homepage</ns2:title>
+  </rdf:Description>
+  <rdf:Description rdf:nodeID="person">
+    <ns1:depiction rdf:resource="http://example.org/pic.jpg"/>
+    <ns1:firstName>Anna</ns1:firstName>
+    <ns1:nick>wildling</ns1:nick>
+    <ns1:nick>wilda</ns1:nick>
+    <ns1:mbox_sha1sum>69e31bbcf58d432950127593e292a55975bc66fd</ns1:mbox_sha1sum>
+    <ns1:surname>Wilder</ns1:surname>
+    <ns1:homepage rdf:resource="http://example.org/about"/>
+    <ns1:made rdf:resource="http://example.org/about"/>
+    <ns1:name>Anna Wilder</ns1:name>
+  </rdf:Description>
+</rdf:RDF>
+
+""" 
