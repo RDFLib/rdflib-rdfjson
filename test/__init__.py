1a2d4c0655079d15fec4a22ce6420ef403e91729
Graham Higgins <gjh@bel-epa.com>
2012-02-06 03:36:27 +0000

    Fix incorrect registration

--- test/__init__.py
+++ test/__init__.py
@@ -1,9 +1,14 @@
 from rdflib import plugin
-from rdflib import store
+from rdflib import serializer
+from rdflib import parser
 import sys # sop to Hudson
 sys.path.insert(0, '/var/lib/tomcat6/webapps/hudson/jobs/rdfextras')
 
 plugin.register(
-        'MySQL', store.Store,
-        'rdfextras.store.MySQL', 'MySQL')
+        'rdfa-json', serializer.Serializer,
+        'rdflib_rdfjson.serializer', 'RdfJsonSerializer')
+
+plugin.register(
+        'rdfa-json', parser.Parser,
+        'rdflib_rdfjson.parser', 'RdfJsonParser')
 
